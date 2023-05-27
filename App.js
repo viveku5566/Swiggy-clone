1f635960f510cfc5a236b8d2431ccd164c5b7bb5
viveku5566 <viveku5566@gmail.com>
2023-05-27 15:33:48 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
  import React from "react";
  import ReactDOM from "react-dom/client";
 
-// React Element 
+const Title = () => (<h1 id="Heading">React using JSX</h1>);
 
-const heading = React.createElement("h1",{id:"heading"},"Hello Vivek");
+const title = (<h1 className="heading">hello vivek this is element</h1>);
+
+const vivek = "Vivek";
+
+const HeadingComponent = () => (
+    <>
+<div id="container">
+        <Title/>
+ <h1>Hello Vivek you're doing Awesome</h1>
+
+ <div id ="container 2">
+    <h2>Hello this is second tag</h2>
+ </div>
+ </div>
+ <div id ="container 3">
+    <h3>This is the third tag</h3>
+ </div>
+ </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
-
+root.render(<HeadingComponent/>);
